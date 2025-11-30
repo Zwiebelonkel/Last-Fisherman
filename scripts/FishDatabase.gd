55fab792c8f6883855d435241a89f6599cbfdd4a
Zwiebelonkel <lucamuller2004@gmail.com>
2025-11-30 18:22:42 +0100

    events

--- scripts/FishDatabase.gd
+++ scripts/FishDatabase.gd
@@ -63,128 +63,128 @@ var FISH_CITY = [
 	{"name": "Sand-Aal", "rarity": RARITY.NORMAL, "base_value": 6, "icon": "res://assets/fish/Aal.png",
 	 "description": "Schlüpfriger als ein Politiker im Wahlkampf."},
 	
-	{"name": "Signalfänger", "rarity": RARITY.NORMAL, "base_value": 7, "icon": "res://assets/fish/signal.png",
-	 "description": "Hat besseren Empfang als dein Handy. Niemand mag den Bastard"},
-	
-	{"name": "Trümmerkabel-Fisch", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 10, "icon": "res://assets/fish/trümmer.png",
-	 "description": "Ernährt sich von HDMI-Kabeln und zerbrochenen Träumen."},
-	
-	{"name": "Dorsch", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 11, "icon": "res://assets/fish/dorsch.png",
-	 "description": "Heißt auch Kabeljau. Hat eine Identitätskrise."},
-	
-	{"name": "Diskusfisch", "rarity": RARITY.SELTEN, "base_value": 18, "icon": "res://assets/fish/diskus.png",
-	 "description": "Flach wie deine Ausreden, aber deutlich bunter."},
-	
-	{"name": "Barrakuda", "rarity": RARITY.SELTEN, "base_value": 25, "icon": "res://assets/fish/baracuda.png",
-	 "description": "Schwimmt schneller als du vor Verantwortung fliehst."},
-	
-	{"name": "Neonflosser", "rarity": RARITY.EPISCH, "base_value": 30, "icon": "res://assets/fish/Eel.png",
-	 "description": "Sieht aus wie ein Rave im Aquarium."},
-	
-	{"name": "Zigaretten", "rarity": RARITY.EPISCH, "base_value": 35, "icon": "res://assets/fish/Boro.png",
-	 "description": "Marlboro Menthol. Seit 1987 im Wasser. Noch immer brennend aktuell."},
-	
-	{"name": "Altstadtgeist", "rarity": RARITY.LEGENDAER, "base_value": 55, "icon": "res://assets/fish/ghost.png",
-	 "description": "Erzählt dir Geschichten von früher. Alle langweilig."},
-	
-	{"name": "Platzhalter", "rarity": RARITY.EXOTISCH, "base_value": 150, "icon": "res://assets/fish/unknown.png",
-	 "description": "Existiert eigentlich nicht. Trotzdem gefangen. Paradox!"},
-	
-	{"name": "Leiche", "rarity": RARITY.EXOTISCH, "base_value": 0, "icon": "res://assets/fish/corpse.png",
-	 "description": "Markus?! Bitte nicht..."},
-	
-	{"name": "Datenkrake", "rarity": RARITY.EXOTISCH, "base_value": 175, "icon": "res://assets/fish/krake.png",
-	 "description": "Kennt deine Suchanfragen. Alle. Ja, auch die. Sicher das der Name richtig ist?"}
+	#{"name": "Signalfänger", "rarity": RARITY.NORMAL, "base_value": 7, "icon": "res://assets/fish/signal.png",
+	 #"description": "Hat besseren Empfang als dein Handy. Niemand mag den Bastard"},
+	#
+	#{"name": "Trümmerkabel-Fisch", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 10, "icon": "res://assets/fish/trümmer.png",
+	 #"description": "Ernährt sich von HDMI-Kabeln und zerbrochenen Träumen."},
+	#
+	#{"name": "Dorsch", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 11, "icon": "res://assets/fish/dorsch.png",
+	 #"description": "Heißt auch Kabeljau. Hat eine Identitätskrise."},
+	#
+	#{"name": "Diskusfisch", "rarity": RARITY.SELTEN, "base_value": 18, "icon": "res://assets/fish/diskus.png",
+	 #"description": "Flach wie deine Ausreden, aber deutlich bunter."},
+	#
+	#{"name": "Barrakuda", "rarity": RARITY.SELTEN, "base_value": 25, "icon": "res://assets/fish/baracuda.png",
+	 #"description": "Schwimmt schneller als du vor Verantwortung fliehst."},
+	#
+	#{"name": "Neonflosser", "rarity": RARITY.EPISCH, "base_value": 30, "icon": "res://assets/fish/Eel.png",
+	 #"description": "Sieht aus wie ein Rave im Aquarium."},
+	#
+	#{"name": "Zigaretten", "rarity": RARITY.EPISCH, "base_value": 35, "icon": "res://assets/fish/Boro.png",
+	 #"description": "Marlboro Menthol. Seit 1987 im Wasser. Noch immer brennend aktuell."},
+	#
+	#{"name": "Altstadtgeist", "rarity": RARITY.LEGENDAER, "base_value": 55, "icon": "res://assets/fish/ghost.png",
+	 #"description": "Erzählt dir Geschichten von früher. Alle langweilig."},
+	#
+	#{"name": "Platzhalter", "rarity": RARITY.EXOTISCH, "base_value": 150, "icon": "res://assets/fish/unknown.png",
+	 #"description": "Existiert eigentlich nicht. Trotzdem gefangen. Paradox!"},
+	#
+	#{"name": "Leiche", "rarity": RARITY.EXOTISCH, "base_value": 0, "icon": "res://assets/fish/corpse.png",
+	 #"description": "Markus?! Bitte nicht..."},
+	#
+	#{"name": "Datenkrake", "rarity": RARITY.EXOTISCH, "base_value": 175, "icon": "res://assets/fish/krake.png",
+	 #"description": "Kennt deine Suchanfragen. Alle. Ja, auch die. Sicher das der Name richtig ist?"}
 ]
 
 var FISH_SEWER = [
 	{"name": "Kackwurst", "rarity": RARITY.NORMAL, "base_value": 8, "icon": "res://assets/fish/kackwurst.png",
 	 "description": "Exakt das, wonach es aussieht. Verkauf es schnell."},
 	
-	{"name": "Sardine", "rarity": RARITY.NORMAL, "base_value": 9, "icon": "res://assets/fish/sardine.png",
-	 "description": "Möchte intensiv in eine Dose gepresst werden."},
-	
-	{"name": "Forelle", "rarity": RARITY.NORMAL, "base_value": 10, "icon": "res://assets/fish/forelle.png",
-	 "description": "Hat schon bessere Tage gesehen. Und bessere Gewässer."},
-	
-	{"name": "Wels", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 12, "icon": "res://assets/fish/Wels.png",
-	 "description": "Sieht aus wie dein Schnurrbart nach drei Wochen Quarantäne."},
-	
-	{"name": "Teerfisch", "rarity": RARITY.SELTEN, "base_value": 20, "icon": "res://assets/fish/teerfisch.png",
-	 "description": "50% Fisch, 50% Industrieabfall, 100% unverdaulich."},
-	
-	{"name": "Mondfisch", "rarity": RARITY.EPISCH, "base_value": 36, "icon": "res://assets/fish/fish1.png",
-	 "description": "Hat die Form einer Vollmondnacht und den IQ eines Kieselsteins."},
-	
-	{"name": "Katzenhai", "rarity": RARITY.EPISCH, "base_value": 40, "icon": "res://assets/fish/Boro.png",
-	 "description": "Miaut unter Wasser. Wissenschaft kann es nicht erklären."},
-	
-	{"name": "Führerschein", "rarity": RARITY.LEGENDAER, "base_value": 80, "icon": "res://assets/fish/führerschein.png",
-	 "description": "Abgelaufen seit 2003. Gehörte vermutlich Rainer, 52."},
-	
-	{"name": "Wütende Scholle", "rarity": RARITY.EXOTISCH, "base_value": 200, "icon": "res://assets/fish/scholle.png",
-	 "description": "Hat valide Gründe für ihre Wut. Glaub mir."},
-	
-	{"name": "Kondom", "rarity": RARITY.EXOTISCH, "base_value": 8, "icon": "res://assets/fish/Kondom.png",
-	 "description": "Gebraucht. Wirf es zurück. WIRF. ES. ZURÜCK."}
+	#{"name": "Sardine", "rarity": RARITY.NORMAL, "base_value": 9, "icon": "res://assets/fish/sardine.png",
+	 #"description": "Möchte intensiv in eine Dose gepresst werden."},
+	#
+	#{"name": "Forelle", "rarity": RARITY.NORMAL, "base_value": 10, "icon": "res://assets/fish/forelle.png",
+	 #"description": "Hat schon bessere Tage gesehen. Und bessere Gewässer."},
+	#
+	#{"name": "Wels", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 12, "icon": "res://assets/fish/Wels.png",
+	 #"description": "Sieht aus wie dein Schnurrbart nach drei Wochen Quarantäne."},
+	#
+	#{"name": "Teerfisch", "rarity": RARITY.SELTEN, "base_value": 20, "icon": "res://assets/fish/teerfisch.png",
+	 #"description": "50% Fisch, 50% Industrieabfall, 100% unverdaulich."},
+	#
+	#{"name": "Mondfisch", "rarity": RARITY.EPISCH, "base_value": 36, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Hat die Form einer Vollmondnacht und den IQ eines Kieselsteins."},
+	#
+	#{"name": "Katzenhai", "rarity": RARITY.EPISCH, "base_value": 40, "icon": "res://assets/fish/Boro.png",
+	 #"description": "Miaut unter Wasser. Wissenschaft kann es nicht erklären."},
+	#
+	#{"name": "Führerschein", "rarity": RARITY.LEGENDAER, "base_value": 80, "icon": "res://assets/fish/führerschein.png",
+	 #"description": "Abgelaufen seit 2003. Gehörte vermutlich Rainer, 52."},
+	#
+	#{"name": "Wütende Scholle", "rarity": RARITY.EXOTISCH, "base_value": 200, "icon": "res://assets/fish/scholle.png",
+	 #"description": "Hat valide Gründe für ihre Wut. Glaub mir."},
+	#
+	#{"name": "Kondom", "rarity": RARITY.EXOTISCH, "base_value": 8, "icon": "res://assets/fish/Kondom.png",
+	 #"description": "Gebraucht. Wirf es zurück. WIRF. ES. ZURÜCK."}
 ]
 
 var FISH_FOREST = [
 	{"name": "Karpfen", "rarity": RARITY.NORMAL, "base_value": 10, "icon": "res://assets/fish/Karpfen.png",
 	 "description": "Meditiert unter Wasser. Unc ist vegatarisch."},
 	
-	{"name": "Wels", "rarity": RARITY.NORMAL, "base_value": 15, "icon": "res://assets/fish/Wels.png",
-	 "description": "Der Bartwuchs ist legendär. Der Rest? Naja."},
-	
-	{"name": "Koi", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 19, "icon": "res://assets/fish/fish1.png",
-	 "description": "Kostet in Japan ein Vermögen. Hier? Mittwochsangebot."},
-	
-	{"name": "Rohrschatten", "rarity": RARITY.SELTEN, "base_value": 27, "icon": "res://assets/fish/fish1.png",
-	 "description": "Versteckt sich zwischen Schilf. Stealth Level 100."},
-	
-	{"name": "Neonflosser", "rarity": RARITY.EPISCH, "base_value": 45, "icon": "res://assets/fish/fish1.png",
-	 "description": "Elektrisierend. Wörtlich."},
-	
-	{"name": "Waldfresser", "rarity": RARITY.EPISCH, "base_value": 70, "icon": "res://assets/fish/Gutter.png",
-	 "description": "Vegetarier. Frisst buchstäblich den Wald. Irgendwie ironisch."},
-	
-	{"name": "Bambushai", "rarity": RARITY.LEGENDAER, "base_value": 100, "icon": "res://assets/fish/fish1.png",
-	 "description": "Ernährt sich von Bambus und innerer Ruhe."},
-	
-	{"name": "Wurzelfisch", "rarity": RARITY.EXOTISCH, "base_value": 250, "icon": "res://assets/fish/fish1.png",
-	 "description": "Halb Fisch, halb Baum, komplett verstörend."},
-	
-	{"name": "Leiche", "rarity": RARITY.EXOTISCH, "base_value": 0, "icon": "res://assets/fish/corpse.png",
-	 "description": "Schon wieder? Ernsthaft?"}
+	#{"name": "Wels", "rarity": RARITY.NORMAL, "base_value": 15, "icon": "res://assets/fish/Wels.png",
+	 #"description": "Der Bartwuchs ist legendär. Der Rest? Naja."},
+	#
+	#{"name": "Koi", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 19, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Kostet in Japan ein Vermögen. Hier? Mittwochsangebot."},
+	#
+	#{"name": "Rohrschatten", "rarity": RARITY.SELTEN, "base_value": 27, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Versteckt sich zwischen Schilf. Stealth Level 100."},
+	#
+	#{"name": "Neonflosser", "rarity": RARITY.EPISCH, "base_value": 45, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Elektrisierend. Wörtlich."},
+	#
+	#{"name": "Waldfresser", "rarity": RARITY.EPISCH, "base_value": 70, "icon": "res://assets/fish/Gutter.png",
+	 #"description": "Vegetarier. Frisst buchstäblich den Wald. Irgendwie ironisch."},
+	#
+	#{"name": "Bambushai", "rarity": RARITY.LEGENDAER, "base_value": 100, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Ernährt sich von Bambus und innerer Ruhe."},
+	#
+	#{"name": "Wurzelfisch", "rarity": RARITY.EXOTISCH, "base_value": 250, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Halb Fisch, halb Baum, komplett verstörend."},
+	#
+	#{"name": "Leiche", "rarity": RARITY.EXOTISCH, "base_value": 0, "icon": "res://assets/fish/corpse.png",
+	 #"description": "Schon wieder? Ernsthaft?"}
 ]
 
 var FISH_DESERT = [
 	{"name": "Schmutzgrundel", "rarity": RARITY.NORMAL, "base_value": 15, "icon": "res://assets/fish/fish1.png",
 	 "description": "Lebt im Dreck. Buchstäblich. Wie deine Ernährungsgewohnheiten."},
 	
-	{"name": "Solarfänger", "rarity": RARITY.NORMAL, "base_value": 16, "icon": "res://assets/fish/signal.png",
-	 "description": "Lädt sich über Sonnenlicht auf. Tesla wäre neidisch."},
-	
-	{"name": "Rostflosser", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 25, "icon": "res://assets/fish/fish1.png",
-	 "description": "Braucht dringend eine Tetanus-Impfung."},
-	
-	{"name": "Sandstreifer", "rarity": RARITY.SELTEN, "base_value": 30, "icon": "res://assets/fish/fish1.png",
-	 "description": "Getarnt wie ein Sandsturm. Mit Flossen."},
-	
-	{"name": "Aggresive Garnele", "rarity": RARITY.EPISCH, "base_value": 55, "icon": "res://assets/fish/fish1.png",
-	 "description": "Winzig, aber verdammt wütend. Wie ein Chihuahua."},
-	
-	{"name": "Staubhai", "rarity": RARITY.EPISCH, "base_value": 80, "icon": "res://assets/fish/Boro.png",
-	 "description": "Schwimmt durch Sand. Physik hat aufgegeben."},
-	
-	{"name": "Wasserfisch", "rarity": RARITY.LEGENDAER, "base_value": 120, "icon": "res://assets/fish/fish1.png",
-	 "description": "Der einzige Fisch mit Wasser in der Wüste. Ein Held."},
-	
-	{"name": "Sonnenfragment", "rarity": RARITY.EXOTISCH, "base_value": 300, "icon": "res://assets/fish/fish1.png",
-	 "description": "Ein Stück Sonne. Anfassen nicht empfohlen."},
-	
-	{"name": "Goldhai", "rarity": RARITY.EXOTISCH, "base_value": 500, "icon": "res://assets/fish/corpse.png",
-	 "description": "Aus echtem Gold. Oder Messing. Vermutlich Messing."}
+	#{"name": "Solarfänger", "rarity": RARITY.NORMAL, "base_value": 16, "icon": "res://assets/fish/signal.png",
+	 #"description": "Lädt sich über Sonnenlicht auf. Tesla wäre neidisch."},
+	#
+	#{"name": "Rostflosser", "rarity": RARITY.UNGEWOEHNLICH, "base_value": 25, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Braucht dringend eine Tetanus-Impfung."},
+	#
+	#{"name": "Sandstreifer", "rarity": RARITY.SELTEN, "base_value": 30, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Getarnt wie ein Sandsturm. Mit Flossen."},
+	#
+	#{"name": "Aggresive Garnele", "rarity": RARITY.EPISCH, "base_value": 55, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Winzig, aber verdammt wütend. Wie ein Chihuahua."},
+	#
+	#{"name": "Staubhai", "rarity": RARITY.EPISCH, "base_value": 80, "icon": "res://assets/fish/Boro.png",
+	 #"description": "Schwimmt durch Sand. Physik hat aufgegeben."},
+	#
+	#{"name": "Wasserfisch", "rarity": RARITY.LEGENDAER, "base_value": 120, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Der einzige Fisch mit Wasser in der Wüste. Ein Held."},
+	#
+	#{"name": "Sonnenfragment", "rarity": RARITY.EXOTISCH, "base_value": 300, "icon": "res://assets/fish/fish1.png",
+	 #"description": "Ein Stück Sonne. Anfassen nicht empfohlen."},
+	#
+	#{"name": "Goldhai", "rarity": RARITY.EXOTISCH, "base_value": 500, "icon": "res://assets/fish/corpse.png",
+	 #"description": "Aus echtem Gold. Oder Messing. Vermutlich Messing."}
 ]
 
 # ===========================
